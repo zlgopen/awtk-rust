9c7c7a865a23049472d61bbec4dbd9a2df63ac72
KunQAQrz <704072809@qq.com>
2025-03-16 18:56:10 +0800

    [awtk_rust_gen] improve code.

--- awtk_rust_gen/src/idl.rs
+++ awtk_rust_gen/src/idl.rs
@@ -1,33 +1,49 @@
-﻿use std::{collections::HashMap, error::Error};
+﻿use serde::Deserialize;
+use std::{collections::HashMap, error::Error};
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Deserialize)]
 pub struct IdlMethodAnnotation {
+    #[serde(default, rename = "static")]
     pub static_: bool,
+
+    #[serde(default)]
     pub constructor: bool,
+
+    #[serde(default)]
     pub deconstructor: bool,
+
+    #[serde(default)]
     pub gc: bool,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Deserialize)]
 pub struct IdlMethod {
     pub name: String,
+
+    #[serde(default)]
     pub annotation: IdlMethodAnnotation,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Deserialize)]
 pub struct IdlClass {
     pub name: String,
+
+    #[serde(default)]
     pub parent: String,
+
+    #[serde(default)]
     pub methods: Vec<IdlMethod>,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Deserialize)]
 pub struct IdlEnum {
     pub name: String,
+
+    #[serde(default)]
     pub prefix: String,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Deserialize)]
 pub struct Idl {
     /* key是类名，value是 IdlClass 对象 */
     pub classes: HashMap<String, IdlClass>,
@@ -45,90 +61,22 @@ impl Idl {
 
     pub fn parse(idl: &str) -> Result<Self, Box<dyn Error>> {
         let mut result: Idl = Idl::new();
-        let parsed = json::parse(idl)?;
+        let items: Vec<serde_json::Value> = serde_json::from_str(idl)?;
 
-        if let json::JsonValue::Array(arr) = parsed {
-            arr.iter().for_each(|item| {
-                if let json::JsonValue::Object(object) = item {
-                    match (
-                        object.get("type").and_then(|t| t.as_str()),
-                        object.get("name").and_then(|n| n.as_str()),
-                    ) {
-                        (Some(type_), Some(name)) => {
-                            if type_ == "class" {
-                                let mut class = IdlClass {
-                                    name: name.into(),
-                                    ..Default::default()
-                                };
-
-                                if let Some(parent) = object.get("parent") {
-                                    class.parent = parent.to_string();
-                                }
-
-                                if let Some(json::JsonValue::Array(methods)) = object.get("methods")
-                                {
-                                    class.methods = methods
-                                        .iter()
-                                        .filter_map(|mth| {
-                                            if let json::JsonValue::Object(m) = mth {
-                                                let mut method = IdlMethod {
-                                                    ..Default::default()
-                                                };
-
-                                                if let Some(name) = m["name"].as_str() {
-                                                    method.name = name.into()
-                                                }
-
-                                                if let Some(json::JsonValue::Object(annotation)) =
-                                                    m.get("annotation")
-                                                {
-                                                    if let Some(static_) =
-                                                        annotation["static"].as_bool()
-                                                    {
-                                                        method.annotation.static_ = static_;
-                                                    }
-                                                    if let Some(constructor) =
-                                                        annotation["constructor"].as_bool()
-                                                    {
-                                                        method.annotation.constructor = constructor;
-                                                    }
-                                                    if let Some(deconstructor) =
-                                                        annotation["deconstructor"].as_bool()
-                                                    {
-                                                        method.annotation.deconstructor =
-                                                            deconstructor;
-                                                    }
-                                                    if let Some(gc) = annotation["gc"].as_bool() {
-                                                        method.annotation.gc = gc;
-                                                    }
-                                                }
-
-                                                Some(method)
-                                            } else {
-                                                None
-                                            }
-                                        })
-                                        .collect();
-                                }
-
-                                result.classes.insert(name.into(), class);
-                            } else if type_ == "enum" {
-                                let mut enum_ = IdlEnum {
-                                    name: name.into(),
-                                    ..Default::default()
-                                };
-
-                                if let Some(prefix) = object.get("prefix") {
-                                    enum_.prefix = prefix.to_string();
-                                }
-
-                                result.enums.insert(name.into(), enum_);
-                            }
-                        }
-                        _ => {}
+        for item in items {
+            if let Some(type_) = item.get("type").and_then(|v| v.as_str()) {
+                match type_ {
+                    "class" => {
+                        let class: IdlClass = serde_json::from_value(item)?;
+                        result.classes.insert(class.name.clone(), class);
                     }
+                    "enum" => {
+                        let enum_: IdlEnum = serde_json::from_value(item)?;
+                        result.enums.insert(enum_.name.clone(), enum_);
+                    }
+                    _ => {}
                 }
-            });
+            }
         }
 
         Ok(result)
