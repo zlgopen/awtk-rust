625443de083dbd9225d3a9dab74bba772f31c049
KunQAQrz <704072809@qq.com>
2025-03-16 22:17:09 +0800

    [awtk_rust_gen] improve exception handling.

--- awtk_rust_gen/src/main.rs
+++ awtk_rust_gen/src/main.rs
@@ -1,24 +1,31 @@
 ﻿use awtk_rust_gen::{args::Args, builder::Builder, idl::Idl};
+use colored::Colorize;
 use std::{fs, process};
+
+macro_rules! err_println {
+    ($($arg:tt)*) => {
+        eprintln!("{}: {}", "error".red().bold(), format!($($arg)*).red());
+    };
+}
 
 fn main() {
     let args = Args::parse().unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}!");
+        err_println!("Problem parsing arguments: {err}!");
         process::exit(1);
     });
 
     let idl_json = fs::read_to_string(&args.idl_path).unwrap_or_else(|err| {
-        println!("Problem reading idl file\"{}\":{err}", args.idl_path);
+        err_println!("Problem reading idl file\"{}\":{err}", args.idl_path);
         process::exit(2);
     });
 
     let idl = Idl::parse(&idl_json).unwrap_or_else(|err| {
-        println!("Problem parsing idl: {err}!");
+        err_println!("Problem parsing idl: {err}!");
         process::exit(3);
     });
 
     Builder::build(&args, &idl).unwrap_or_else(|err| {
-        println!("Problem building: {err}!");
+        err_println!("Problem building: {err}!");
         process::exit(4);
     });
 
